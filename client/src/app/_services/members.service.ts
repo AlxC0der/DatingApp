8498aad85b6997fb9f4e91bbe909e7e3e319c1e2
AlxC0der <alejandroyael22@hotmail.com>
2024-11-05 12:10:37 -0600

    Parcial03 - Update member update in SPA

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,10 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { Member } from '../_models/member';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { Member } from "../_models/member";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MembersService {
   private http = inject(HttpClient);
@@ -16,5 +16,9 @@ export class MembersService {
 
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + "users/" + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + "users", member);
   }
 }
