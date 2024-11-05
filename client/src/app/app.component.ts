86659fca53f0eaa155e4705709bae5f26073b797
AlxC0der <alejandroyael22@hotmail.com>
2024-11-05 11:56:59 -0600

    Parcial03 - Update member edit component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
-import { AccountService } from "./_services/account.service";
+import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent, HomeComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css"
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
-  title = "Date me";
+  title = 'Date me';
 
   ngOnInit(): void {
     this.setCurrentUser();
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,14 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Member } from '../../_models/member';
-import { AccountService } from '../../_services/account.service';
-import { MembersService } from '../../_services/members.service';
+import { Component, inject, OnInit } from "@angular/core";
+import { Member } from "../../_models/member";
+import { AccountService } from "../../_services/account.service";
+import { MembersService } from "../../_services/members.service";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-member-edit',
+  selector: "app-member-edit",
   standalone: true,
-  imports: [],
-  templateUrl: './member-edit.component.html',
-  styleUrl: './member-edit.component.css'
+  imports: [TabsModule, FormsModule],
+  templateUrl: "./member-edit.component.html",
+  styleUrl: "./member-edit.component.css"
 })
 export class MemberEditComponent implements OnInit {
   member?: Member;
@@ -23,7 +25,7 @@ export class MemberEditComponent implements OnInit {
     const user = this.accountService.currentUser();
     if (!user) return;
     this.membersService.getMember(user.username).subscribe({
-      next: member => this.member = member
-    })
+      next: (member) => (this.member = member)
+    });
   }
 }
