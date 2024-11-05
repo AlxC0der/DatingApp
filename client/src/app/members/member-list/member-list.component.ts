0b351e270d1efa5ddc86f91a022ce477bb01f1b2
AlxC0der <alejandroyael22@hotmail.com>
2024-11-05 09:29:50 -0600

    Parcial03 - Add members support

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { MembersService } from "../../_services/members.service";
+import { Member } from "../../_models/member";
 
 @Component({
-  selector: "app-members-list",
+  selector: "app-member-list",
   standalone: true,
   imports: [],
   templateUrl: "./member-list.component.html",
   styleUrl: "./member-list.component.css"
 })
-export class MembersListComponent {}
+export class MemberListComponent implements OnInit {
+  private membersService = inject(MembersService);
+  members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers().subscribe({
+      next: (members) => (this.members = members)
+    });
+  }
+}
