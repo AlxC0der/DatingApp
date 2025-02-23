290cf9afb1c3881a28f7df5a1d6a19bfe4fd201d
AlxC0der <alejandroyael22@hotmail.com>
2025-02-22 21:16:28 -0600

    Parcial04-Fixes

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,7 @@ public class Startup
         services.AddApplicationServices(_config);
         services.AddControllers();
         services.AddIdentityServices(_config);
+    
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
