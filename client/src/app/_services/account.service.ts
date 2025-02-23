8391ae8662a4141fc29c85eac464536a3b7b7e25
AlxC0der <alejandroyael22@hotmail.com>
2025-02-22 19:59:20 -0600

    Parcial014 - Add photo to nav bar

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,11 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { User } from '../_models/user';
-import { environment } from '../../environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+import { map, Observable } from "rxjs";
+import { User } from "../_models/user";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AccountService {
   private http = inject(HttpClient);
@@ -16,8 +16,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
       map((user) => {
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -27,12 +26,16 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
       map((user) => {
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
     );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem("user", JSON.stringify(user));
+    this.currentUser.set(user);
   }
 
   logout(): void {
