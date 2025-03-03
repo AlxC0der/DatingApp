d0d18cc8e449aff163299109ed9516cf8384d4ba
AlxC0der <alejandroyael22@hotmail.com>
2025-03-03 07:56:37 -0600

    Parcial05 - Add pagination classes

--- API/Helpers/PaginationHeader.cs
+++ API/Helpers/PaginationHeader.cs
@@ -6,4 +6,5 @@ public class PaginationHeader(int currentPage, int itemsPerPage, int totalItems,
     public int ItemsPerPage { get; set; } = itemsPerPage;
     public int TotalItems { get; set; } = totalItems;
     public int TotalPages { get; set; } = totalPages;
+    //First Commit
 }
