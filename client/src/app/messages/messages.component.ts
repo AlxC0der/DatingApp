7f007d6d0daa56665a1071d27df6358bfc4f77f0
AlxC0der <alejandroyael22@hotmail.com>
2025-04-13 19:27:55 -0600

    Parcial05 - Add MessagesService to SPA

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from "@angular/core";
+import { MessagesService } from "../_services/messages.service";
 
 @Component({
-  selector: 'app-messages',
+  selector: "app-messages",
   standalone: true,
   imports: [],
-  templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+  templateUrl: "./messages.component.html",
+  styleUrl: "./messages.component.css"
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
+  container = "outbox";
+  pageNumber = 1;
+  pageSize = 10;
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
